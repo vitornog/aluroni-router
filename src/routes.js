c2a5a36a5e4bb7ed24b76247922233a24fa8620a
Vitor Hugo Nogueira <5271@inb.gov.br>
2022-03-17 14:12:17 -0300

    .

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import Menu from 'Components/Menu';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import styles from './routes.module.scss';
+
+export default function AppRouter(){
+	return(
+		<main>
+			<Router>
+				<Menu />
+                
+				<header className={styles.header}>
+					<div className={styles.header__text}>
+                        A casa do código e da massa
+					</div>
+				</header>
+            
+				<Routes>
+					<Route path='/' element={<Inicio/>} />
+					<Route path='/cardapio' element={<Cardapio/>} />
+				</Routes>
+			</Router>
+		</main>
+	);
+}
